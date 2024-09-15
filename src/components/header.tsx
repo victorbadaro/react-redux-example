45f4ed593d910db5a6184cfd8075c037e8d24929
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-15 17:18:09 -0300

    chore: change header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 export function Header() {
 	return (
-		<header className="flex justify-between py-4 px-8 bg-zinc-950 text-zinc-100">
-			<span className="text-2xl">React Redux Example</span>
-			<button type="button" className="py-1 px-2 rounded text-sm font-semibold bg-lime-400 text-lime-900 hover:opacity-80 transition">New Item</button>
+		<header className="flex items-center h-20 bg-zinc-950 text-zinc-100">
+			<nav className="flex justify-between items-center mx-auto w-11/12">
+				<span className="text-2xl">React Redux Example</span>
+				<button type="button" className="px-4 h-10 rounded-lg text-sm font-semibold bg-lime-400 text-lime-950 hover:bg-lime-500 transition-colors">New Item</button>
+			</nav>
 		</header>
 	);
 }
