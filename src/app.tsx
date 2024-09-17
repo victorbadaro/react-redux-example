5d61b3119cb567860311367c577fe7baa1a263cc
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-17 12:14:43 -0300

    feat: add an element to show on the screen when the users list is empty

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,27 @@
 import { Header } from './components/header';
 import { UsersList } from './components/users-list';
-import { UsersProvider } from './providers/users-provider';
+import { useUsers } from './hooks/use-users';
 
 export function App() {
+	const { users } = useUsers();
+
 	return (
 		<div className="space-y-8 min-h-screen bg-zinc-900 text-zinc-100">
-			<UsersProvider>
-				<Header />
+			<Header />
 
-				<main className="container mx-auto">
-					<div className="flex flex-col gap-4">
-						<h1 className="text-2xl font-semibold">Users</h1>
+			<main className="container mx-auto">
+				<div className="flex flex-col gap-4">
+					<h1 className="text-2xl font-semibold">Users</h1>
+
+					{users.length > 0 ? (
 						<UsersList />
-					</div>
-				</main>
-			</UsersProvider>
+					) : (
+						<div className="flex items-center justify-center h-72 border border-dashed border-zinc-700 rounded-lg">
+							<p>Users list is empty.</p>
+						</div>
+					)}
+				</div>
+			</main>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
 
 import './index.css';
+import { UsersProvider } from './providers/users-provider.tsx';
 
 const container = document.getElementById('root');
 
-createRoot(container!).render(<App />);
+createRoot(container!).render(
+	<UsersProvider>
+		<App />
+	</UsersProvider>
+);
